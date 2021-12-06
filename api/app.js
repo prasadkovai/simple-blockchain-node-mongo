dc74cca78060012799c5c00bc1b2d704422a14ea
PRASAD P <prasad.p@changecx.com>
2021-12-06 18:41:16 +0530

    api data from MongoDB

---
+++ api/app.js
@@ -0,0 +1,51 @@
+const Express = require("express");
+const BodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+const CONNECTION_URL = 'mongodb+srv://prasad:w1eyAe5f7Mz2iUW5@prasad.ycxrx.mongodb.net';
+const DATABASE_NAME = "test_blockchain";
+
+
+var app = Express();
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+var database, collection;
+
+app.listen(5000, () => {
+    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        database = client.db(DATABASE_NAME);
+        collection = database.collection("audit_log_chain");
+        console.log("Connected to `" + DATABASE_NAME + "`!");
+    });
+});
+
+app.get("/alluser", (request, response) => {
+    collection.find({}).toArray((error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(JSON.stringify(result));
+    });
+});
+
+app.post("/user", (request, response) => {
+    console.log(request, response,'Get-Method');
+    collection.insert(request.body, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result.result);
+    });
+});
+
+app.get("/user/:id", (request, response) => {
+    collection.findOne({ "_id": new ObjectId(request.params.id) }, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
